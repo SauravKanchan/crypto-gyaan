77d1db9c2321f8aa14dca0463f156844e9a26410
SauravKanchan <sauravnk30@gmail.com>
2019-10-10 18:58:35 +0530

    Add Order event

--- contracts/CryptoGyaan.sol
+++ contracts/CryptoGyaan.sol
@@ -11,15 +11,17 @@ contract CryptoGyaan{
         erc721 = MyERC721(_gyann_contract);
     }
 
-    struct Order{
-        string _token_id;
-        address seller;
-    }
+    event Order(
+        uint256 indexed _token_id,
+        address indexed seller,
+        uint256 price
+    );
 
-    function deposit(uint256 _token_id) public returns (bool) {
+    function deposit(uint256 _token_id, uint256 _price) public returns (bool) {
         erc721.transferFrom(msg.sender, address(this), _token_id);
         require(erc721.ownerOf(_token_id)==address(this), "Token not deposited");
         balances[msg.sender].push(_token_id);
+        emit Order(_token_id, msg.sender, _price);
         return true;
     }
 
