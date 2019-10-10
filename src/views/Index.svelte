50ce5ef6cef7c9291fba4ca028f6597ce2679f9b
SauravKanchan <sauravnk30@gmail.com>
2019-10-11 01:55:26 +0530

    Add sveltestrap

---
+++ src/views/Index.svelte
@@ -0,0 +1,10 @@
+<script>
+  import { Button, Col, Row } from 'sveltestrap';
+</script>
+
+<Row>
+  <Col>
+    <Button color="primary" outline>Hello World!</Button>
+  </Col>
+</Row>
+<a href="/something">Something</a>
