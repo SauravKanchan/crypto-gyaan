26f4f3571429f2bdfba31db943ef5cdcdf17f608
SauravKanchan <sauravnk30@gmail.com>
2019-10-10 18:16:15 +0530

    first commit

---
+++ contracts/erc721.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.5.0;
+
+import "https://raw.githubusercontent.com/OpenZeppelin/openzeppelin-contracts/master/contracts/token/ERC721/ERC721Full.sol";
+
+contract MyERC721 is ERC721Full {
+
+    constructor (string memory _name, string memory _symbol) public
+    ERC721Full(_name, _symbol)
+    {
+    }
+
+    function mintUniqueTokenTo(
+        address _to,
+        string  memory _tokenURI
+    ) public
+    {
+        uint256 _tokenId = super.totalSupply();
+        super._mint(_to, _tokenId);
+        super._setTokenURI(_tokenId, _tokenURI);
+    }
+}
