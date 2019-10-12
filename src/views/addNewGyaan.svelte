2cff95885f13de79072fb07a1965dac2662d0838
SauravKanchan <sauravnk30@gmail.com>
2019-10-12 15:19:07 +0530

    Fix image upload to ipfs

--- src/views/addNewGyaan.svelte
+++ src/views/addNewGyaan.svelte
@@ -1,36 +1,31 @@
 <script>
     let image = "";
+    const axios = require('axios');
+    let status = "";
+    let image_path = "";
+    let hash = "";
 
-    async function upload(evt) {
-// canvas
 
-        var canvas = document.getElementById('canvas');
-        var context = canvas.getContext("2d");
-        var fileinput = document.getElementById('photo'); // input file
-        var img = new Image();
+    async function upload(e) {
+        if (this.files && this.files[0]) {
+            var FR= new FileReader();
 
-            var files = evt.target.files; // FileList object
-            var file = files[0];
-            if(file.type.match('image.*')) {
-                var reader = new FileReader();
-                // Read in the image file as a data URL.
-                reader.readAsDataURL(file);
-                reader.onload = async function(evt){
-                    if( evt.target.readyState == FileReader.DONE) {
-                        img.src = evt.target.result;
-                        context.drawImage(img,100,100);
-                        console.log(await ipfs.add(canvas.toDataURL()))
-                    }
-                }
+            FR.addEventListener("load", async function(e) {
+                let base64 = e.target.result;
+                status = "Adding image to IPFS...";
+                let hash = await ipfs.add(base64);
+                status = "Added image to IPFS "+hash;
+                let res = await axios.get("https://ipfs.io/ipfs/"+hash);
+                image = res.data;
+            });
 
-            } else {
-                alert("not an image");
-            }
+            FR.readAsDataURL( this.files[0] );
+        }
     }
 </script>
 
 <div class="d-flex justify-content-center m-4 row">
-    <div class="h2 col-md-6 offset-md-3">Add New Gyaan</div>
+    <div class="h2 col-md-6 offset-md-3">Add New Gyaan {image_path}</div>
 
     <div class="card col-md-6">
         <form class="m-5">
@@ -45,10 +40,25 @@
             </div>
             <div class="form-group">
                 <label for="pwd">Image: </label>
-                <input type="file" name="photo" id="photo" class="form-control" bind:value={image} on:change={upload}>
+                <input type="file" name="photo" id="photo" class="form-control" bind:value={image_path} on:change={upload}>
             </div>
             <button type="submit" class="btn btn-primary">Submit</button>
         </form>
     </div>
 </div>
-<canvas id="canvas" width="5" height="5"></canvas>
+{#if status}
+    <div class="d-flex justify-content-center row ">
+        <div class="col-md-2">
+            <div class="h2">
+                status
+            </div>
+        </div>
+        <div class="col-md-10 text-truncate">
+            {status}
+        </div>
+        <div class="col-md-6 offset-md-3">
+            <img src={image} class="img-fluid" alt="image"/>
+        </div>
+    </div>
+{/if}
+<canvas id="canvas" style="display: none"></canvas>
